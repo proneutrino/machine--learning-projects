5af348027ac1d425f5e2a5b47d9d1a0d74a8610e
Faheemuddin Rumaan Shaik <32872170+proneutrino@users.noreply.github.com>
2018-05-30 09:25:11 +0530

    Update svm_author_id.py

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -21,10 +21,42 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 
 
+from sklearn.svm import SVC
+clf = SVC(kernel="rbf",C=10000)
 
-#########################################################
-### your code goes here ###
 
-#########################################################
+#### now your job is to fit the classifier
+#### using the training features/labels, and to
+#### make a set of predictions on the test data
+t0 = time()
+clf.fit(features_train,labels_train)
+print "training time:", round(time()-t0, 3), "s"
 
+
+
+#### store your predictions in a list named pred
+t0 = time()
+preds = clf.predict(features_test)
+print "predicting time:", round(time()-t0, 3), "s"
+print preds[10]
+print preds[26]
+print preds[50]
+
+
+counter=0
+
+
+"""count of chris predictions"""
+for pred in preds:
+	if pred==1:
+		counter=counter+1
+
+
+print counter
+
+
+from sklearn.metrics import accuracy_score
+acc = accuracy_score(preds, labels_test)
+
+print(acc)
 
