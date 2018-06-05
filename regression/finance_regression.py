d996688278de511ffce6451e4cd8c528531c6e11
Faheemuddin Rumaan Shaik <32872170+proneutrino@users.noreply.github.com>
2018-06-05 06:47:25 +0530

    Completed Regression

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -1,18 +1,5 @@
 #!/usr/bin/python
-
-"""
-    Starter code for the regression mini-project.
     
-    Loads up/formats a modified version of the dataset
-    (why modified?  we've removed some trouble points
-    that you'll find yourself in the outliers mini-project).
-
-    Draws a little scatterplot of the training/testing data
-
-    You fill in the regression code where indicated:
-"""    
-
-
 import sys
 import pickle
 sys.path.append("../tools/")
