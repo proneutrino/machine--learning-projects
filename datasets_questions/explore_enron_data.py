a133d33cf02e689de3b3cc2034f0fbf31686b9e1
Faheemuddin Rumaan Shaik <32872170+proneutrino@users.noreply.github.com>
2018-06-05 14:57:34 +0530

    Completed datasets_questions

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python
 
-""" 
-    Starter code for exploring the Enron dataset (emails + finances);
-    loads up the dataset (pickled dict of dicts).
-
-    The dataset has the form:
-    enron_data["LASTNAME FIRSTNAME MIDDLEINITIAL"] = { features_dict }
-
-    {features_dict} is a dictionary of features associated with that person.
-    You should explore features_dict as part of the mini-project,
-    but here's an example to get you started:
-
-    enron_data["SKILLING JEFFREY K"]["bonus"] = 5600000
-    
-"""
-
 import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
+sume=0
 
+for counter in enron_data["SKILLING JEFFREY K"]:
+	sume=sume+1
+
+print(sume)
+
+counter=0
+
+for person_name in enron_data:
+	if enron_data[person_name]["poi"]==1:
+		counter = counter+1
+
+print(counter)
+
+
+print enron_data["PRENTICE JAMES"]["total_stock_value"]
+print enron_data["COLWELL WESLEY"]["from_this_person_to_poi"]
+print enron_data["SKILLING JEFFREY K"]["exercised_stock_options"]
